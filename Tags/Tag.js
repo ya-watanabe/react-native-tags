f6971baa9ab15b932c01a135d6f3237f7038d8d0
ya-watanabe <yasuo.watanabe@muzou.jp>
2019-03-14 13:24:23 +0900

    add delete icon temprary; note: should be replace to pass component

--- Tags/Tag.js
+++ Tags/Tag.js
@@ -1,26 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
 
-const Tag = ({ label, onPress, tagContainerStyle, tagTextStyle, readonly }) => {
+const Tag = ({
+  label,
+  onPress,
+  tagContainerStyle,
+  tagTextStyle,
+  readonly,
+  hasDeleteIcon = true
+}) => {
   const tagText = <Text style={[styles.tagLabel, tagTextStyle]}>{label}</Text>;
 
   if (readonly) {
-    return (
-      <View style={[styles.tag, tagContainerStyle]}>
-        {tagText}
-      </View>
-    )
+    return <View style={[styles.tag, tagContainerStyle]}>{tagText}</View>;
   } else {
     return (
-      <TouchableOpacity style={[styles.tag, tagContainerStyle]} onPress={onPress}>
+      <TouchableOpacity
+        style={[styles.tag, tagContainerStyle]}
+        onPress={onPress}
+      >
         {tagText}
+        {hasDeleteIcon && (
+          <Ionicons
+            size={14}
+            name={"ios-close-circle-outline"}
+            color={"rgba(0, 0, 0, 0.87)"}
+            style={{ padding: 4 }}
+          />
+        )}
       </TouchableOpacity>
-    )
+    );
   }
-}
+};
 
 Tag.propTypes = {
   label: PropTypes.string.isRequired,
